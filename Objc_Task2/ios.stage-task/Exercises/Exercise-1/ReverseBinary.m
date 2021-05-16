d9e8e93bb9efd94ce9d69d070e3b24dbd5546f54
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-05-16 19:51:27 +0300

    feat: add solution for exercise-1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,18 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+
+    int result = 0;
+    int lastBit = 0;
+    
+    
+    for(int i = 0; i < 8; i++) {
+
+        lastBit = n & 1;
+        n = n >> 1;
+        result = (result << 1) | lastBit;
+    }
+    
+    return result;
 }
