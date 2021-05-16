1f19111f7303d2b1ae5c6980806803bd5a91d90a
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-05-16 20:27:44 +0300

    feat: add solution for exercise-2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,19 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    int counter = 0;
+    
+    for (NSUInteger i = array.count - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            
+            if ((array[i].intValue - array[j].intValue) == number.intValue) {
+                counter++;
+            }
+        }
+    }
+    
+    return counter;
 }
 
 @end
