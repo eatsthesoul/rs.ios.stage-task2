9fd4d80a54f61e8abad4115e494f729d06955fa1
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-05-17 10:45:53 +0300

    feat: add solution for exercise-3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,41 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    NSString *address = @"";
+    
+    if (numbersArray.count > 0) {
+        
+        NSNumber *number;
+        
+        for (int i = 0; i < 4; i++) {
+            
+            //handling case when the number of elements in numbersArray is less than 4.
+            if (numbersArray.count < i + 1) {
+                number = @0;
+            } else {
+                //good case
+                number = numbersArray[i];
+            }
+            
+            //exception handling
+            if (number.intValue > 255) {
+                return @"The numbers in the input array can be in the range from 0 to 255.";
+            }
+            
+            //exception handling
+            if (number.intValue < 0) {
+                return @"Negative numbers are not valid for input.";
+            }
+            
+            address = [address stringByAppendingFormat:@"%@.", number];
+        }
+        
+        //remove "." in the end of the adress
+        address = [address substringToIndex:[address length] - 1];
+    }
+    
+    return address;
 }
 
 @end
